ff26b6ab8163db4cdb13189edbdf0fd57b0fcd0a
Thompson Naidu <thompsonnaiducm1343@gmail.com>
2023-10-03 01:58:50 -0400

    adding configuration to give 503 error if the database is down

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import appRoute from './routes/app.route.js';
 import assingmentRoute from "./routes/assignment.route.js";
+import databaseConnectionMiddleware from './middlewares/databaseConnectionMiddleware.js';
 const app=express();
 
 const PORT= process.env.PORT || 3000;
 app.use(express.json());
-
+//check if the database connection is available
+app.use(databaseConnectionMiddleware);
 app.use("/healthz",appRoute);
-app.use("/v1/assignment",assingmentRoute);
+app.use("/v1/assignments",assingmentRoute);
 app.use("/",(req,res)=>res.status(503).send())
 app.listen(PORT,(err)=>{
     if(err){
